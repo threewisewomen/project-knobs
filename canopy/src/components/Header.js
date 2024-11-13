bf5b9b7f2389ff147b7da393c1672ca538e39ea7
John Mathew <jmathew@acilogistix.com>
2024-11-13 13:08:54 +0530

    navbar implemented

---
+++ canopy/src/components/Header.js
@@ -0,0 +1,14 @@
+// src/components/Header.js
+import React from 'react';
+import Navbar from './Navbar';
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
--- canopy/src/pages/HomePage.js
+++ canopy/src/pages/HomePage.js
@@ -1,14 +1,17 @@
 // src/pages/HomePage.js
 import React from 'react';
-import Navbar from '../components/Navbar';
+import Header from '../components/Header';
+import '../styles/HomePage.css';
 
 const HomePage = () => {
   return (
-    <div>
-      <Navbar />
-      <h1>Welcome to Project Knobs</h1>
-      <p>Customize your guitar knobs with us!</p>
-      {/* Add more content for your homepage here */}
+    <div className="homepage-container">
+      <Header />
+      <h1 className="homepage-title">Welcome to Project Knobs</h1>
+      <p className="homepage-subtitle">Customize your guitar knobs with us!</p>
+      <div className="homepage-content">
+        {/* Additional homepage content here */}
+      </div>
     </div>
   );
 };
